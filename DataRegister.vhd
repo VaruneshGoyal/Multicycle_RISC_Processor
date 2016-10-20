f5d50a33b3f3ef5827bc07ba4e3e495cd4495cb7
prakirt <prakirt2203@gmail.com>
2016-10-20 17:07:45 +0530

    data register initialization

--- DataRegister.vhd
+++ DataRegister.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 entity DataRegister is
 	generic (data_width:integer);
 	port (Din: in std_logic_vector(data_width-1 downto 0);
-	      Dout: out std_logic_vector(data_width-1 downto 0);
+	      Dout: out std_logic_vector(data_width-1 downto 0) := (others => '0');
 	      clk, enable: in std_logic);
 end entity;
 architecture Behave of DataRegister is
