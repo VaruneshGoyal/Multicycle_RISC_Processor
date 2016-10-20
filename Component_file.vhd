0f59b0e5f6bd067f51a4315d6eac3721626b1352
prakirt <prakirt2203@gmail.com>
2016-10-20 17:28:20 +0530

    update dataregister

--- Component_file.vhd
+++ Component_file.vhd
@@ -26,7 +26,7 @@ end component;
 component DataRegister is
 	generic (data_width:integer);
 	port (Din: in std_logic_vector(data_width-1 downto 0);
-	      Dout: out std_logic_vector(data_width-1 downto 0);
+	      Dout: out std_logic_vector(data_width-1 downto 0) := (others => '0');
 	      clk, enable: in std_logic);
 end component;
 
